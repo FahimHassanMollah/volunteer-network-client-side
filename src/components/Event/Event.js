436488f67faf2b35701433798448e805eefe9c2f
FahimHassanMollah <fahimhassan0197@gmail.com>
2020-10-04 17:45:20 +0600

    event page fully completed data retived and also delete  api added in this event page

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -6,28 +6,56 @@ import UserEventsDetails from '../UserEventsDetails/UserEventsDetails';
 
 const Event = () => {
     const [user, setUser] = useContext(LoggedInUserContext);
-    const [userEventsInformation, setUserEventsInformation] = useState([])
+    const [userEventsInformation, setUserEventsInformation] = useState([]);
+    const [reloeadPageAfterDeleteEvent, setReloeadPageAfterDeleteEvent] = useState(0);
+    const [allCategories, setAllCategories] = useState([])
+    const deleteEvent = (id) => {
+        console.log('working click ' + id);
+        fetch('http://localhost:8080/deleteEvent/' + id, {
+            method: 'DELETE',
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8'
+            },
+
+        })
+            .then(res => res.json())
+            .then(res => {
+                setReloeadPageAfterDeleteEvent(reloeadPageAfterDeleteEvent + 1);
+
+            })
+    }
+
     useEffect(() => {
-      fetch('http://localhost:8080/volunterEventsInformationByEmail/'+user.email)
-      .then(res=>res.json())
-      .then(data=>{
-        setUserEventsInformation(data);
-      })
-    }, [user.email])
+        fetch('http://localhost:8080/volunterEventsInformationByEmail/' + user.email)
+            .then(res => res.json())
+            .then(data => {
+                setUserEventsInformation(data);
+            })
+
+
+    }, [user.email, reloeadPageAfterDeleteEvent])
+    useEffect(() => {
+
+        fetch('http://localhost:8080/getVolunteerCategories')
+            .then(res => res.json())
+            .then(res => {
+                setAllCategories(res)
+            })
+    }, [])
     return (
         <div>
             <Header></Header>
             <Container>
-                <Row className="d-flex justify-content-center">
-                   
-                      
-                     
-                      {
-                            userEventsInformation.length>0 &&userEventsInformation.map((event)=> <UserEventsDetails userEvents={event}></UserEventsDetails>)
-                        }
-                     
-                       
-                 
+                <Row className="d-flex justify-content-center pt-5">
+
+
+
+                    {
+                        allCategories.length > 0 && userEventsInformation.length > 0 && userEventsInformation.map((event, index) => <UserEventsDetails allCategories={allCategories} key={index} deleteEvent={deleteEvent} userEvents={event}></UserEventsDetails>)
+                    }
+
+
+
                 </Row>
             </Container>
 
--- src/components/UserEventsDetails/UserEventsDetails.js
+++ src/components/UserEventsDetails/UserEventsDetails.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import { Col } from 'react-bootstrap';
+import { Button, Col, Image } from 'react-bootstrap';
 
-const UserEventsDetails = () => {
+const UserEventsDetails = (props) => {
+    const {_id,names,description,category,date}=props.userEvents;
+    let allCategoriesInformations=props.allCategories;
+   let checker= allCategoriesInformations.find((singleCategory)=>singleCategory.categoryName===category);
+   
     return (
         <div>
             <Col md={12}>
-            <div className="card mb-3 " style={{maxWidth:"400px"}}>
-                <div className="row no-gutters">
-                    <div className="col-md-6">
-                        <svg className="bd-placeholder-img" width="100%" height="250" xmlns="http://www.w3.org/2000/svg" aria-label="Placeholder: Image" preserveAspectRatio="xMidYMid slice" role="img"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96" /><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image</text></svg>
+            <div className="card pb-3 border-0 " style={{maxWidth:"400px"}}>
+                <div className="row no-gutters" style={{ boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}}>
+                    <div className="col-md-5 p-2" style={{ boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}}>
+                      <img style={{width:'100%'}} src={checker.image} alt=""/>
 
                     </div>
-                    <div className="col-md-6">
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">It's a broader card with text below as a natural lead-in to extra content. This content is a little longer.</p>
-                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                    <div className="col-md-7">
+                        <div className="card-body p-2">
+                            <h5 className="card-title">{category}</h5>
+                            <p className="card-text"><small className="text-muted">date:{date}</small></p>
+                            <Button onClick={()=>props.deleteEvent(_id)} className="btn-danger">delete</Button>
+                           
                         </div>
                     </div>
                 </div>
